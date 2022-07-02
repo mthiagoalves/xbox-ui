107664a5ca2ba8d490242187272980b554fd4a10
mthiagoalves <m.thiago.ales@live.com>
2022-07-02 18:13:49 +0100

    fix(game & genre): nest partial for swagger partialtypes

--- src/game/dto/update-game.dto.ts
+++ src/game/dto/update-game.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
+
+import { PartialType } from '@nestjs/swagger';
 import { CreateGameDto } from './create-game.dto';
 
 export class UpdateGameDto extends PartialType(CreateGameDto) {}
