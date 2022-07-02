55116b10790b85cc170be10e8906e6c84c35a7ca
mthiagoalves <m.thiago.ales@live.com>
2022-07-02 16:30:52 +0100

    chore(game & genre): error catchs implement

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { error } from 'console';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { Game } from './dto/entities/game.entity';
@@ -12,34 +17,57 @@ export class GameService {
     return this.prisma.game.findMany();
   }
 
-  findOne(id: string): Promise<Game> {
-    return this.prisma.game.findUnique({
+  async findById(id: string): Promise<Game> {
+    const record = await this.prisma.game.findUnique({
       where: {
         id,
       },
     });
+
+    if (!record) {
+      throw new NotFoundException(`Id '${id}' is not found, please check`);
+    }
+
+    return record;
+  }
+
+  findOne(id: string): Promise<Game> {
+    return this.findById(id);
   }
 
   create(createGameDto: CreateGameDto): Promise<Game> {
     const game: Game = { ...createGameDto };
 
-    return this.prisma.game.create({
-      data: game,
-    });
+    return this.prisma.game
+      .create({
+        data: game,
+      })
+      .catch(this.handleError);
   }
 
-  update(id: string, updateGameDto: UpdateGameDto): Promise<Game> {
+  async update(id: string, updateGameDto: UpdateGameDto): Promise<Game> {
+    await this.findById(id);
+
     const data: Partial<Game> = { ...updateGameDto };
 
-    return this.prisma.game.update({
-      where: { id },
-      data,
-    });
+    return this.prisma.game
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
   }
 
   async delete(id: string) {
     await this.prisma.game.delete({
       where: { id },
     });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1].trim();
+
+    throw new UnprocessableEntityException(lastErrorLine);
   }
 }
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGenreDto } from './dto/create-genre.dto';
 import { UpdateGenreDto } from './dto/update-genre.dto';
@@ -12,32 +16,55 @@ export class GenreService {
     return this.prisma.genre.findMany();
   }
 
-  findOne(id: string): Promise<Genre> {
-    return this.prisma.genre.findUnique({
+  async findById(id: string): Promise<Genre> {
+    const record = await this.prisma.genre.findUnique({
       where: { id },
     });
+
+    if (!record) {
+      throw new NotFoundException(`Id '${id}' is not found, please check`);
+    }
+    return record;
+  }
+
+  findOne(id: string): Promise<Genre> {
+    return this.findById(id);
   }
 
   create(createGenreDto: CreateGenreDto): Promise<Genre> {
     const genre: Genre = { ...createGenreDto };
 
-    return this.prisma.genre.create({
-      data: genre,
-    });
+    return this.prisma.genre
+      .create({
+        data: genre,
+      })
+      .catch(this.handleError);
   }
 
-  update(id: string, updateGenreDto: UpdateGenreDto): Promise<Genre> {
+  async update(id: string, updateGenreDto: UpdateGenreDto): Promise<Genre> {
+    await this.findById(id);
+
     const genre: Partial<Genre> = { ...updateGenreDto };
 
-    return this.prisma.genre.update({
-      where: { id },
-      data: genre,
-    });
+    return this.prisma.genre
+      .update({
+        where: { id },
+        data: genre,
+      })
+      .catch(this.handleError);
   }
 
   async delete(id: string) {
+    await this.findById(id);
     await this.prisma.genre.delete({
       where: { id },
     });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1].trim();
+
+    throw new UnprocessableEntityException(lastErrorLine);
   }
 }
